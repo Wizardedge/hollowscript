2509598c11408e54e5e58494b9ff55a3b855913c
thrwiz <8900432-wizcab@users.noreply.gitlab.com>
2021-06-03 08:40:24 +0000

    未対応のCATEGORY_GRAVE_SPSUMMON修正

--- c32768230.lua
+++ c32768230.lua
@@ -31,6 +31,7 @@ function c32768230.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(c32768230.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,c32768230.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,0,0,0)
 end
 function c32768230.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
