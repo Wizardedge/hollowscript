181f86dc52cb56d97895490b40ac4ab56c2bdcef
ｔｈｒ <wizcab@gmail.com>
2021-07-18 19:28:57 +0900

    add,IsExtraDeckMonster

--- c30126992.lua
+++ c30126992.lua
@@ -55,7 +55,7 @@ function c30126992.tdop(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then
 			Duel.SendtoDeck(tc,nil,1,REASON_EFFECT)
 		else
-			if tc:IsType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ+TYPE_LINK)
+			if tc:IsExtraDeckMonster()
 				or Duel.SelectOption(tp,aux.Stringid(30126992,1),aux.Stringid(30126992,2))==0 then
 				Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)
 			else
