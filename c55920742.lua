813b38d70eeb913d446d0908fa42a7d150242bdd
ｔｈｒ <wizcab@gmail.com>
2021-11-05 16:35:25 +0900

    fix

--- c55920742.lua
+++ c55920742.lua
@@ -70,7 +70,8 @@ function c55920742.spop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local tg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,1,nil)
 	if tg:GetCount()>0 then
-		if Duel.SendtoGrave(tg,REASON_EFFECT)==0 then return end
+		local tc=tg:GetFirst()
+		if Duel.SendtoGrave(tg,REASON_EFFECT)==0 or not tc:IsLocation(LOCATION_GRAVE) then return end
 		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,c55920742.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
@@ -83,8 +84,7 @@ function c55920742.rctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
-	local dam=bc:GetAttack()
-	if dam<0 then dam=0 end
+	local dam=bc:GetBaseAttack()
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(dam)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,dam)
