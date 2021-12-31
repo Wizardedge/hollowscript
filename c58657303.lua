9dc4414c02d82833b212e8042b1a98c30d6272fc
ｔｈｒ <wizcab@gmail.com>
2021-12-31 12:54:13 +0900

    fix

--- c58657303.lua
+++ c58657303.lua
@@ -1,63 +1,51 @@
---ドロドロゴン
-function c84040113.initial_effect(c)
-	--synchro summon
-	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
-	c:EnableReviveLimit()
-	--fusion (s)
+--墓守の霊術師
+function c58657303.initial_effect(c)
+	--fusion summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,84040113)
-	e1:SetCondition(c84040113.spcon)
-	e1:SetTarget(c84040113.sptg)
-	e1:SetOperation(c84040113.spop)
+	e1:SetCountLimit(1,58657303)
+	e1:SetCondition(c58657303.condition)
+	e1:SetTarget(c58657303.target)
+	e1:SetOperation(c58657303.operation)
 	c:RegisterEffect(e1)
-	--fusion substitute
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_FUSION_SUBSTITUTE)
-	e2:SetCondition(c84040113.subcon)
-	c:RegisterEffect(e2)
 end
-function c84040113.subcon(e)
-	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+function c58657303.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsEnvironment(47355498)
 end
-function c84040113.spcon(e)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+function c58657303.filter1(c,e)
+	return not c:IsImmuneToEffect(e)
 end
-function c84040113.spfilter1(c,e)
-	return c:IsOnField() and not c:IsImmuneToEffect(e)
-end
-function c84040113.spfilter2(c,e,tp,m,f,gc,chkf)
-	return c:IsType(TYPE_FUSION) and (not f or f(c))
+function c58657303.filter2(c,e,tp,m,f,gc,chkf)
+	return c:IsType(TYPE_FUSION) and c:IsRace(RACE_SPELLCASTER) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,gc,chkf)
 end
-function c84040113.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c58657303.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)
-		local res=Duel.IsExistingMatchingCard(c84040113.spfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,c,chkf)
+		local mg1=Duel.GetFusionMaterial(tp)
+		local res=Duel.IsExistingMatchingCard(c58657303.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,c,chkf)
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
 				local fgroup=ce:GetTarget()
 				local mg2=fgroup(ce,e,tp)
 				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c84040113.spfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,c,chkf)
+				res=Duel.IsExistingMatchingCard(c58657303.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,c,chkf)
 			end
 		end
 		return res
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c84040113.spop(e,tp,eg,ep,ev,re,r,rp)
+function c58657303.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local chkf=tp
 	if not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
-	local mg1=Duel.GetFusionMaterial(tp):Filter(c84040113.spfilter1,nil,e)
-	local sg1=Duel.GetMatchingGroup(c84040113.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c,chkf)
+	local mg1=Duel.GetFusionMaterial(tp):Filter(c58657303.filter1,nil,e)
+	local sg1=Duel.GetMatchingGroup(c58657303.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c,chkf)
 	local mg2=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -65,7 +53,7 @@ function c84040113.spop(e,tp,eg,ep,ev,re,r,rp)
 		local fgroup=ce:GetTarget()
 		mg2=fgroup(ce,e,tp)
 		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c84040113.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c,chkf)
+		sg2=Duel.GetMatchingGroup(c58657303.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c,chkf)
 	end
 	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
 		local sg=sg1:Clone()
