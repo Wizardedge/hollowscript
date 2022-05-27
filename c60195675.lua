79814b5c8b61330223bee993108fea35040acd50
thr <wizcab@gmail.com>
2022-05-28 02:54:30 +0900

    fix

--- c60195675.lua
+++ c60195675.lua
@@ -40,7 +40,7 @@ function c60195675.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c60195675.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
